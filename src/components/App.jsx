6b4fc970a1a98203eafc356df6d41e9cfb722705
Chris W <christopherkwildenradt@gmail.com>
2018-06-27 14:29:02 -0700

    App is now a class

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,32 @@
-var App = () => {
-
-  var handleButtonClick = function(event) {
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  handleButtonClick(event) {
     event.preventDefault();
     console.log('clicked');
     //change a state in the app to clear the input
   }
-
-  return (
-    <div>
-      <nav className="navbar">
-        <div className="col-md-6 offset-md-3">
-          <div> <Search videos={window.exampleVideoData} /></div>
-        </div>
-      </nav>
-      <div className="row">
-        <div className="col-md-7">
-          <div> <VideoPlayer videos={window.exampleVideoData} /></div>
-        </div>
-        <div className="col-md-5">
-          <div> <VideoList videos={window.exampleVideoData} /></div>
+  render() {
+    return (
+      <div>
+        <nav className="navbar">
+          <div className="col-md-6 offset-md-3">
+            <div> <Search videos={window.exampleVideoData} /></div>
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-md-7">
+            <div> <VideoPlayer videos={window.exampleVideoData} /></div>
+          </div>
+          <div className="col-md-5">
+            <div> <VideoList videos={window.exampleVideoData} /></div>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
+
 }
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
