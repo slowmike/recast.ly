e92bf7649689a2327cffa8255be1ce832c880d1d
slowmike <aznwizard123@gmail.com>
2018-06-27 19:35:10 -0700

    Get search youtube to work

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,10 +1,22 @@
-var searchYouTube = (options, callback) => {
-  $.ajax({
-    data: {
-      maxResults: 5,
-      q: options,
-      key: window.YOUTUBE_API_KEY,
 
+var searchYouTube = (options, callback) => {
+
+
+  $.ajax({
+    type: 'GET',
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    data: {
+      maxResults: options.max,
+      q: options.query,
+      key: options.key,
+      part: 'snippet',
+      type: 'video',
+      videoEmbeddable: true
+    },
+    success: function(data) {
+      callback(data.items);
+    },
+    error: function(error) {
     }
   });
 };
